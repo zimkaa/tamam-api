134687e71ae0337928056ee753edb77b5ed4b0fb
Anton Zimin <zimkaa87@gmail.com>
2023-02-24 19:04:14 +0300

    new version
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- back_end/settings.py
+++ back_end/settings.py
@@ -9,25 +9,31 @@ load_dotenv()
 
 
 CHANNEL_ID = os.getenv("CHANNEL_ID")
+print(f"{CHANNEL_ID=}")
 
 TG_TOKEN = os.getenv("TG_TOKEN")
+print(f"{TG_TOKEN=}")
 
 TG_URL = os.getenv("TG_URL")
 
 APP_NAME = os.getenv("APP_NAME", "Code checker")
 
 DB_USER = os.getenv("DB_USER")
+print(f"{DB_USER=}")
 
 DB_PASS = os.getenv("DB_PASS")
 
 DB_HOST = os.getenv("DB_HOST")
+print(f"{DB_HOST=}")
 
 DB_NAME = os.getenv("DB_NAME")
 
 DB_PORT = os.getenv("DB_PORT")
+print(f"{DB_PORT=}")
 
 # connect string for the real database
 REAL_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(f"{REAL_DATABASE_URL=}")
 
 TEST_DB_USER = os.getenv("TEST_DB_USER", "postgres_test")
 
@@ -46,6 +52,7 @@ DIGISELLER_TOKEN = os.getenv("DIGISELLER_TOKEN", "DIGISELLER_TOKEN NOT IN GLOBAL
 print(f"{DIGISELLER_TOKEN=}")
 
 DIGISELLER_ID = os.getenv("DIGISELLER_ID")
+print(f"{DIGISELLER_ID=}")
 
 APILOGIN_URL = os.getenv("APILOGIN_URL", "https://api.digiseller.ru/api/apilogin")
 
