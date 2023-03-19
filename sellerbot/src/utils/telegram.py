8d1f53f2ad38baffb62e7aeb4e34d2ace9d1418f
Anton Zimin <zimkaa87@gmail.com>
2023-03-19 13:12:48 +0300

    dell unused
    Signed-off-by: Anton Zimin <zimkaa87@gmail.com>

--- sellerbot/src/utils/telegram.py
+++ sellerbot/src/utils/telegram.py
@@ -2,30 +2,15 @@ import httpx
 from loguru import logger
 
 from src.settings import ADMIN_CHANNEL_ID
-from src.settings import CHANNEL_ID
 from src.settings import TG_TOKEN
 from src.settings import TG_URL
-
-
-# async def send_telegram_message(text: str) -> None:
-#     async with httpx.AsyncClient() as client:
-#         method = TG_URL + TG_TOKEN + "/sendMessage"  # type: ignore
-#         response = await client.post(
-#             method,
-#             data={
-#                 "chat_id": CHANNEL_ID,
-#                 "text": text,
-#             },
-#         )
-#         if response.status_code != 200:
-#             raise Exception("Some trouble with TG")
 
 
 async def send_telegram_message(text: str) -> None:
     async with httpx.AsyncClient() as client:
         method = TG_URL + TG_TOKEN + "/sendMessage"  # type: ignore
         logger.debug("send_telegram_message:")
-        lst_id = ADMIN_CHANNEL_ID.split(",")
+        lst_id = ADMIN_CHANNEL_ID.split(",")  # type: ignore
         for chanel_id in lst_id:
             logger.debug(f"{chanel_id=}")
             query = await client.post(
